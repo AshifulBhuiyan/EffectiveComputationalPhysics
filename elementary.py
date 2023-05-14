ce4d2decf8ec4d6d12347a7352390b73fbc917ad
AshifulBhuiyan <ashiful.b@gmail.com>
2023-05-13 22:29:17 -0400

    ch6 twerk2

---
+++ elementary.py
@@ -0,0 +1,6 @@
+# elementary.py
+class ElementaryParticle(Particle):
+    def __init__(self, spin):
+    self.s = spin
+    self.is_fermion = bool(spin % 1.0)
+    self.is_boson = not self.is_fermion
--- particle.py
+++ particle.py
@@ -1,4 +1,9 @@
 # particle.py
+from scipy import constants
+
+def possible_flavors():
+    return["up","down","top","bottom","strange","charm"]
+
 class Particle(object):
     """A particle is a constituent unit of the universe.
     128
@@ -10,22 +15,32 @@ class Particle(object):
     m : mass in units of [kg]
     r : position in units of [meters]
     """
-roar = "I am a particle!"
+    roar = "I am a particle!"
 
-def __init__(self, charge, mass, position):
-    """Initializes the particle with supplied values for
-    charge c, mass m, and position r.
-    """
-    self.c = charge
-    self.m = mass
-    self.r = position
+    def __init__(self, charge, mass, position):
+        """Initializes the particle with supplied values for
+        charge c, mass m, and position r.
+        """
+        self.c = charge
+        self.m = mass
+        self.r = position
 
-def hear_me(self):
-    myroar = self.roar + (
-        "My charge is:      " + str(self.c) +
-        "My mass is:        " + str(self.m) +
-        "My x position is:  " + str(self.r['x']) + 
-        "My y position is:  " + str(self.r['y']) + 
-        "My z position is:  " + str(self.r['z'])
-    )
-    print(myroar)
+    def hear_me(self):
+        myroar = self.roar + (
+            "\n My charge is:      " + str(self.c) +
+            "\n My mass is:        " + str(self.m) +
+            "\n My x position is:  " + str(self.r['x']) + 
+            "\n My y position is:  " + str(self.r['y']) + 
+            "\n My z position is:  " + str(self.r['z'])
+        )
+        print(myroar)
+    
+    def delta_x_min(self, delta_p_x):
+        hbar = constants.hbar
+        delx_min = hbar / (2.0 * delta_p_x)
+        return delx_min
+    @staticmethod
+    def possible_flavors():
+        return ["up", "down", "top", "bottom", "strange", "charm"]
+    
+    
